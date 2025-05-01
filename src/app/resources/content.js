027110f49152543c79fb78023476d164af854f38
Pawan0603 <www.pawanmthakre@gmail.com>
2025-05-01 23:43:22 +0530

    first commit

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,15 +1,15 @@
 import { InlineCode } from "@/once-ui/components";
 
 const person = {
-  firstName: "Selene",
-  lastName: "Yu",
+  firstName: "Pawan",
+  lastName: "Thakre",
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
-  role: "Design Engineer",
-  avatar: "/images/avatar.jpg",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  role: "Web Developer",
+  avatar: "/images/avatar3.png",
+  location: "India/Maharashtra", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["Marathi", "English", "Hindi"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter = {
@@ -29,22 +29,22 @@ const social = [
   {
     name: "GitHub",
     icon: "github",
-    link: "https://github.com/once-ui-system/nextjs-starter",
+    link: "https://github.com/Pawan0603",
   },
   {
     name: "LinkedIn",
     icon: "linkedin",
-    link: "https://www.linkedin.com/company/once-ui/",
+    link: "https://www.linkedin.com/in/pawan-thakre-12b99a2b8/",
   },
   {
     name: "X",
     icon: "x",
-    link: "",
+    link: "https://x.com/pawanmthakre",
   },
   {
     name: "Email",
     icon: "email",
-    link: "mailto:example@gmail.com",
+    link: "mailto:pawanthakre252002@gmail.com",
   },
 ];
 
@@ -52,11 +52,11 @@ const home = {
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
-  headline: <>Design engineer and builder</>,
+  headline: <>Web Developer</>,
   subline: (
     <>
-      I'm Selene, a design engineer at <InlineCode>FLY</InlineCode>, where I craft intuitive
-      <br /> user experiences. After hours, I build my own projects.
+      I'm Pawan, a web developer. I build performant and responsive web applications.
+      <br /> Find me on <a target="blank_" href="https://x.com/pawanmthakre">twitter</a>
     </>
   ),
 };
@@ -74,16 +74,15 @@ const about = {
   },
   calendar: {
     display: true,
-    link: "https://cal.com",
+    link: "https://x.com/pawanmthakre",
   },
   intro: {
     display: true,
     title: "Introduction",
     description: (
       <>
-        Selene is a Jakarta-based design engineer with a passion for transforming complex challenges
-        into simple, elegant design solutions. Her work spans digital interfaces, interactive
-        experiences, and the convergence of design and technology.
+        Hi, I'm Pawan — a Web Developer crafting seamless digital experiences using modern web technologies.
+        With hands-on expertise in Next.js, React.js, and the modern web stack, I build performant and responsive web applications. I've delivered projects for clients and also work on personal projects to sharpen my skills and explore new ideas in web development.
       </>
     ),
   },
@@ -91,46 +90,57 @@ const about = {
     display: true, // set to false to hide this section
     title: "Work Experience",
     experiences: [
+      // {
+      //   company: "FLY",
+      //   timeframe: "2022 - Present",
+      //   role: "Senior Design Engineer",
+      //   achievements: [
+      //     <>
+      //       Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user
+      //       engagement and 30% faster load times.
+      //     </>,
+      //     <>
+      //       Spearheaded the integration of AI tools into design workflows, enabling designers to
+      //       iterate 50% faster.
+      //     </>,
+      //   ],
+      //   images: [
+      //     // optional: leave the array empty if you don't want to display images
+      //     {
+      //       src: "/images/projects/project-01/cover-01.jpg",
+      //       alt: "Once UI Project",
+      //       width: 16,
+      //       height: 9,
+      //     },
+      //   ],
+      // },
+      // {
+      //   company: "Creativ3",
+      //   timeframe: "2018 - 2022",
+      //   role: "Lead Designer",
+      //   achievements: [
+      //     <>
+      //       Developed a design system that unified the brand across multiple platforms, improving
+      //       design consistency by 40%.
+      //     </>,
+      //     <>
+      //       Led a cross-functional team to launch a new product line, contributing to a 15% increase
+      //       in overall company revenue.
+      //     </>,
+      //   ],
+      //   images: [],
+      // },
       {
-        company: "FLY",
-        timeframe: "2022 - Present",
-        role: "Senior Design Engineer",
+        company: "Freelance",
+        timeframe: "2023 - Present",
+        role: "Web Devloper",
         achievements: [
           <>
-            Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user
-            engagement and 30% faster load times.
-          </>,
-          <>
-            Spearheaded the integration of AI tools into design workflows, enabling designers to
-            iterate 50% faster.
+            Project development freelancer for clients, including students and professionals. Adept at delivering high-quality, tailor-made projects that meet specific client requirements within established deadlines.
           </>,
         ],
-        images: [
-          // optional: leave the array empty if you don't want to display images
-          {
-            src: "/images/projects/project-01/cover-01.jpg",
-            alt: "Once UI Project",
-            width: 16,
-            height: 9,
-          },
-        ],
-      },
-      {
-        company: "Creativ3",
-        timeframe: "2018 - 2022",
-        role: "Lead Designer",
-        achievements: [
-          <>
-            Developed a design system that unified the brand across multiple platforms, improving
-            design consistency by 40%.
-          </>,
-          <>
-            Led a cross-functional team to launch a new product line, contributing to a 15% increase
-            in overall company revenue.
-          </>,
-        ],
-        images: [],
-      },
+        images: []
+      }
     ],
   },
   studies: {
@@ -138,12 +148,14 @@ const about = {
     title: "Studies",
     institutions: [
       {
-        name: "University of Jakarta",
-        description: <>Studied software engineering.</>,
+        name: "Gondwana University",
+        // timeframe: "2019 - 2022",
+        description: <>Studied Master of Science in Computer Science.</>,
       },
       {
-        name: "Build the Future",
-        description: <>Studied online marketing and personal branding.</>,
+        name: "Gondwana University",
+        timeframe: "2020 - 2023",
+        description: <>Studied Bachelor of science in Computer Science.</>,
       },
     ],
   },
@@ -151,37 +163,73 @@ const about = {
     display: true, // set to false to hide this section
     title: "Technical skills",
     skills: [
+      // {
+      //   title: "Figma",
+      //   description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
+      //   // optional: leave the array empty if you don't want to display images
+      //   images: [
+      //     {
+      //       src: "/images/projects/project-01/cover-02.jpg",
+      //       alt: "Project image",
+      //       width: 16,
+      //       height: 9,
+      //     },
+      //     {
+      //       src: "/images/projects/project-01/cover-03.jpg",
+      //       alt: "Project image",
+      //       width: 16,
+      //       height: 9,
+      //     },
+      //   ],
+      // },
+      // { 
+      //   title: "Next.js",
+      //   description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
+      //   // optional: leave the array empty if you don't want to display images
+      //   images: [
+      //     {
+      //       src: "/images/projects/project-01/cover-04.jpg",
+      //       alt: "Project image",
+      //       width: 16,
+      //       height: 9,
+      //     },
+      //   ],
+      // },
       {
-        title: "Figma",
-        description: <>Able to prototype in Figma with Once UI with unnatural speed.</>,
-        // optional: leave the array empty if you don't want to display images
-        images: [
-          {
-            src: "/images/projects/project-01/cover-02.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-          {
-            src: "/images/projects/project-01/cover-03.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-        ],
+        title: "HTML/CSS",
+        description: <>Building responsive and performant web apps with HTML/CSS.</>,
+      },
+      {
+        title: "JavaScript",
+        description: <>Building performant web apps with JavaScript.</>,
+      },
+      {
+        title: "React.js",
+        description: <>Building interactive UIs with component-based architecture.</>,
       },
       {
         title: "Next.js",
-        description: <>Building next gen apps with Next.js + Once UI + Supabase.</>,
-        // optional: leave the array empty if you don't want to display images
-        images: [
-          {
-            src: "/images/projects/project-01/cover-04.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-        ],
+        description: <>Building full-stack web applications with Next.js and server-side rendering..</>,
+      },
+      {
+        title: "Tailwind CSS",
+        description: <>Building responsive and performant web apps with Tailwind CSS.</>,
+      },
+      // {
+      //   title: "Supabase",
+      //   description: <>Building performant web apps with Supabase.</>,
+      // },
+      {
+        title: "Node.js",
+        description: <> Developing backend logic and APIs using JavaScript runtime.</>,
+      },
+      {
+        title: "Express.js",
+        description: <> Building RESTful APIs and server-side apps with minimal overhead.</>,
+      },
+      {
+        title: "MongoDB",
+        description: <>Designing NoSQL databases with flexible document-based structure.</>,
       },
     ],
   },
