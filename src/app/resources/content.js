42aeb57b21935e64d93f9ed35cd90f21cd616b61
Pawan0603 <www.pawanmthakre@gmail.com>
2025-11-30 02:35:04 +0530

    fix location format

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -8,7 +8,7 @@ const person = {
   },
   role: "Web Developer",
   avatar: "/images/avatar.png",
-  location: "India/Maharashtra", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  location: "Maharashtra/India", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["Marathi", "English", "Hindi"], // optional: Leave the array empty if you don't want to display languages
 };
 
